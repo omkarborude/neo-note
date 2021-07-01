011fe0e419adeec2fef97e0d0bbf3fd39f427687
omkar <borudeomkar41@gmail.com>
2021-07-01 18:23:34 +0530

    Refactore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Landing, Navbar, Home, EditNote, Account } from "./Components/index";
+import { useDispatch, useSelector } from "react-redux";
+import { GetAllNotes } from "./Slice/NotesSlice";
+import { Route, Routes } from "react-router";
+import React, { useEffect } from "react";
+import { PrivateRoute } from "./utils";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const loginData = useSelector((state) => state.auth);
+  const notes = useSelector((state) => state.note);
+  const currentUser = loginData.login;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (currentUser) {
+      axios.defaults.headers.common["authorization"] = currentUser.token;
+    } else {
+      delete axios.headers.common["authorization"];
+    }
+  }, [currentUser]);
+
+  useEffect(() => {
+    dispatch(GetAllNotes());
+  }, [currentUser]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <PrivateRoute path="/home" element={<Home />} />
+        <PrivateRoute path="/editnote/:noteId" element={<EditNote />} />
+        <PrivateRoute path="/account" element={<Account />} />
+      </Routes>
     </div>
   );
 }
